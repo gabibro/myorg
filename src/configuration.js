c82a24d161c67d18bdd9b05f76cf651f0f3c02b4
gabi_bro_ <lolopepexd9@gmail.com>
2020-08-21 21:03:09 -0300

    Modificacion datos

--- src/configuration.js
+++ src/configuration.js
@@ -1,13 +1,13 @@
 exports.token = { //Claves requeridas para el funcionamiento de la API.
-	key: 'de2aab7fe42c85de24b324e90c737887a1512694',
-	privated: 'a4ce4e15ecd9df7e8fbfd9dc95d06e0c42e18f5d'
+	key: 'key-here',
+	privated: 'private-here'
 }
 
 exports.discordBot = { //Claves y configuraciones extras del cliente.
-	token: 'NzQ1NDUyOTA5MzczODE2ODkz.Xzx_Hw.BLnGLZSiDIP9mFauwNDW3AtD4yM',
+	token: 'token-here',
 	prefix: 'my!',
 	embedColor: 'BLUE',
-	embedImage: 'https://cdn.discordapp.com/icons/245624094161108993/a_074e0d42e410fad556b09eb92e1d9391.webp'
+	embedImage: 'image-here'
 }
 
-exports.mongoURI = 'mongodb://localhost:27017/myorg' //URI de conexion para base de datos.
+exports.mongoURI = 'mongoURI-here' //URI de conexion para base de datos.
