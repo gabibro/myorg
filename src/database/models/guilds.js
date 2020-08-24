a1599db3c313634e0321ba1fb1579f9dea3ea574
gabi_bro_ <lolopepexd9@gmail.com>
2020-08-24 17:24:11 -0300

    Modelo 'guilds' actualizado

--- src/database/models/guilds.js
+++ src/database/models/guilds.js
@@ -3,7 +3,7 @@ let { Schema, model } = require('mongoose')
 let guildSchema = new Schema({
 	guildID: { type: String, required: true },
 	prefix: { type: String, required: true, default: require('../../configuration').discordBot.prefix },
-	plataform: { type: String, required: true },
+	plataform: { type: String, required: true, default: 'samp' },
 	organization: { type: Object, required: true, default: {
 		has: false,
 		type: 'org',
