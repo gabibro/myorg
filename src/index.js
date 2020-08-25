f600ab805f37be3b86a1e21f97d98a535d103c42
gabi_bro_ <lolopepexd9@gmail.com>
2020-08-25 15:52:39 -0300

    Cambio de apodo agregado

--- src/index.js
+++ src/index.js
@@ -94,7 +94,11 @@ client.on('guildMemberAdd', async (member) => {
   let role = member.guild.roles.cache.get(userRank.role_id)
   let roleGeneral = findOrg.roles.find(r => r.name === findOrg.name)
 
-  member.roles.add([role, roleGeneral.role_id])
+  let name = `${SAMPuserInfo.name.split('_')[0]} ${SAMPuserInfo.name.split('_')[1]}`
+
+  member.setNickname(name).then(() => {
+     member.roles.add([role, roleGeneral.role_id])
+  })
 
   }
 
