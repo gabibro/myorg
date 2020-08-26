e0c0dd31a9d4b90595fd7d384dc0ad981adb4d28
gabi_bro_ <lolopepexd9@gmail.com>
2020-08-25 22:33:46 -0300

    Modificacion

--- src/database/index.js
+++
@@ -1,4 +0,0 @@
-let mongo = require('mongoose')
-let { mongoURI } = require('../configuration')
-
-module.exports = async() => { await mongo.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }) }
